d4e4f11ce816622ad88b759db92093aa5490905c
Nick <nicolas.pettican@postgrad.manchester.ac.uk>
2016-08-04 02:51:56 +0100

    finished (for now)

--- PDBparser.py
+++ PDBparser.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import re
-import pdb
-
 #########################
 #                       #
 #      PDB-parser       #
@@ -13,11 +8,17 @@ import pdb
 #                       #
 #########################
 
+import os
+import sys
+import re
+
 # optional user input file location
-#DATADIR = raw_input("\nInput file location: ")
-#DATAFILE = raw_input("\nInput file name: ")
+#DATADIR = raw_input("\nINPUT FILE LOCATION: ")
+#DATAFILE = raw_input("\nINPUT FILE NAME: ")
+#DATAOUT = raw_input("\nOUTPUT FILE NAME: ")
 DATADIR = "F:\MSc\RP2\DSGONgly"
 DATAFILE = "new1001.pdb"
+OUTFILE = "new1001pymod.pdb"
 
 
 def parse(datafile):
@@ -27,7 +28,7 @@ def parse(datafile):
 
 def welcome():
     # opening credits
-    print ("#" * 25 + "\n#\t\t\t#\n#\tPDB-parser\t#" + "\n#\t\t\t#" + 
+    print ("#" * 25 + "\n#\t\t\t#\n#\tPDB-parser\t#" + "\n#\t\t\t#" +
            "\n#  by Nicolas Pettican  #" + "\n#\t\t\t#\n" + "#" *25 + "\n")
 
 def lineheads(datafile):
@@ -47,16 +48,20 @@ def optionsbegin(pdb):
     # options the user has
     choice = int
     while not (choice == 1 or choice == 2 or choice == 3):
-        choice = input("What would you like to do to the PDB file?\n" + 
-                       "Here are the current options:\n" + 
-                       "1. Replace a number or string    # it will replace every instance of that number or string\n" + 
-                       "2. Nothing else yet...           # there will be more options in the future\n" + 
+        choice = input("What would you like to do to the PDB file?\n" +
+                       "Here are the current options:\n" +
+                       "1. Replace a number or string    # it will replace every instance of that number or string\n" +
+                       "2. Nothing else yet...           # there will be more options in the future\n" +
                        "3. Quit\n")
     return choice
 
 def findelement(pdb,busca):
     # finds where the elements to change are
     return sum(1 for l in pdb if re.search(busca, l))
+
+def findrange(pdb,busca):
+    rangebusca = [i for i, line in enumerate(pdb) if re.search(busca, line)]
+    return [rangebusca[0], rangebusca[-1] + 2]
 
 def findindex(pdb,busca):
     # finds what column the query string is
@@ -74,7 +79,8 @@ def replacechar(pdb):
         while not (len(busca) > 2):
             busca = raw_input("\n*Note that it must be 3 or more characters*\t")
         instances = findelement(pdb,busca)
-        print "\n%s appears %s times" %(busca, instances)
+        rangebusca = findrange(pdb,busca)
+        print "\n%s appears %s times in lines %s to %s" %(busca, instances, rangebusca[0], rangebusca[-1])
     except:
         print "\nWoops, can't seem to find %s\n" %(busca)
     # finds what column the query string is in
@@ -94,83 +100,104 @@ def replacechar(pdb):
         cambia = raw_input("Insert number or string you want to replace it with:\t")
         if len(busca) == len(cambia):
             newpdb = replace(pdb,busca,cambia)
-            #print newpdb[0:2]
             return newpdb
         else:
-            print ("\nYou can't replace a string of different size...\n" + 
+            print ("\nYou can't replace a string of different size...\n" +
                    "it will break the PDB file!\n")
             quit()
     except:
         print "\nWoops, something went wrong\n"
         quit()
-        
+
 def replace(pdb,busca,cambia):
     # THE REPLACING FUNCTION, under development
     # note that this will break the pdb into individual characters
     # pdbchars has all the lines broken into individual characters
     # for some reason it struggles with busca <= 3
-    
+
     pdbchars = [[l.strip() for l in line] for line in pdb]
     buscachars = [l.strip() for l in busca]
     cambiachars = [l.strip() for l in cambia]
+    rangebusca = findrange(pdb,busca)
     
     begin = 100   # just as a default
     end = 101     # just as a default
-    
+
+    # I really don't like nested code...
+    # but it could be worse!
     newpdb = []
     counter = 0
     for line in pdbchars:
         newline = []
         b = 0
         charcount = 0
-        for char in line:
-            check = 0
-            if line[charcount] == buscachars[0] and line[charcount + 1] == buscachars[1]:
-                for i, value in enumerate(buscachars):
-                    if line[charcount + i] == buscachars[i]:
-                        check = i
-                if check > 1: 
-                    begin = b
-                    end = b + len(buscachars)
-                    x = 0
-                    for chars in pdbchars[counter][begin:end]:
-                        newline.append(char.replace(char, cambiachars[x]))
-                        x += 1
-            elif charcount not in range(begin, end):
+        if counter not in range(rangebusca[0], rangebusca[-1]):
+            for char in line:
                 newline.append(char)
-            charcount += 1
-            b += 1
+        else:
+            for char in line:
+                check = 0
+                if line[charcount] == buscachars[0] and line[charcount + 1] == buscachars[1]:
+                    for i, value in enumerate(buscachars):
+                        if line[charcount + i] == buscachars[i]:
+                            check = i
+                    if check > 1:
+                        begin = b
+                        end = b + len(buscachars)
+                        x = 0
+                        for chars in pdbchars[counter][begin:end]:
+                            newline.append(char.replace(char, cambiachars[x]))
+                            x += 1
+                elif charcount not in range(begin, end):
+                    newline.append(char)
+                charcount += 1
+                b += 1
         counter += 1
         newpdb.append(newline)
-        
+
     return newpdb
 
-def reconstruct(pdb,newpdb):
+def reconstruct(newpdb):
     # reconstruct the "replaced" lines with the appropriate spacing
-    pass
+    readypdb = []
+    for line in newpdb:
+        newline = []
+        for char in line:
+            if char != '':
+                newline.append(char)
+            elif char == '':
+                newline.append(' ')
+        readypdb.append(newline)
+    return ["".join(str(x) for x in line) for line in readypdb]
+
+def outputfile(readypdb,outfile):    
+    with open(outfile, 'w') as output:
+        output.writelines(str(i) + "\n" for i in readypdb)
+        #output.write('\n'.join(str(i)) for i in readypdb)
 
 def nothingyet():
     # just some of my nonesense :grin:
-    print ("\nNot much else going on here for now,\n" + 
+    print ("\nNot much else going on here for now,\n" +
            "I want to add a 'replace entire column' option,\n" +
            "but I could use some help to increase functionality,\n" +
            "so be sure to fork it on github :D\n")
-    sys.exit("\n\nQuitting the session...\n" + "-"*20 + 
+    sys.exit("\n\nQuitting the session...\n" + "-"*20 +
              "\nThank you for using PDB-parser!\n")
 
 def continuar():
     # the "are you sure?" function
     return raw_input("\nContinue? [yes|no]\t").lower().startswith('y')
-    
+
 def quit():
     # quite, of course
-    sys.exit("\n\nQuitting the session...\n" + "-"*20 + 
-             "\nThank you for using PDB-parser!\n" + "-"*20 + 
-             "\nBe sure to check it out on GitHub: nickpettican/PDB-parse\n")
+    sys.exit("\n\nQuitting the session...\n" + "-"*20 +
+             "\nThank you for using PDB-parser!\n" + "-"*20 +
+             "\nBe sure to check it out on GitHub:\ngithub.com/nickpettican/PDB-parse\n")
 
 def main():
     # the main function
     datafile = os.path.join(DATADIR, DATAFILE)
+    outfile = os.path.join(DATADIR, OUTFILE)
     if datafile:
         # pdb is now a list containing all the lines of the input PDB file
         pdb = parse(datafile)
@@ -179,16 +206,28 @@ def main():
         choice = optionsbegin(pdb)
         if choice == 1:
             newpdb = replacechar(pdb)
-        if choice == 2:
+        elif choice == 2:
             nothingyet()
-        if choice == 3:
+        elif choice == 3:
             quit()
-        
-        print newpdb[0:5]
-        
+
+        try: 
+            readypdb = reconstruct(newpdb)
+            print "\n*Successfuly reconstructed new PDB structure!*\n"
+        except:
+            print "\nWoops, something went wrong when reconstructing the PDB!\n"
+            quit()
+
+        try:
+            outputfile(readypdb,outfile)
+            print "*Successfuly created new PDB file!*\n"
+        except:
+            print "\nWoops, something went wrong when creating the PDB!\n"
+            quit()
+   
     else:
         print "\nCould not open file\nMake sure you type the correct directory and file name\n"
         quit()
-    
+
 if __name__ == "__main__":
     main()
