edc3c4a56c58289dc973ba0bb280fff523065d8b
Nick Pettican <nicolas.pettican@postgrad.manchester.ac.uk>
2016-08-11 10:54:04 +0100

    added ATOM to HETATM (v/v)

--- PDBparser.py
+++ PDBparser.py
@@ -18,10 +18,12 @@ from operator import itemgetter
 #DATADIR = raw_input("\nINPUT FILE LOCATION: ")
 #DATAFILE = raw_input("\nINPUT FILE NAME: ")
 #OUTFILE = raw_input("\nOUTPUT FILE NAME: ")
-DATADIR = "F:\MSc\RP2\PyTests"
-DATAFILE = "test02.pdb"
-OUTFILE = "testmod02.pdb"
-
+DATADIR = "G:\MSc\RP2\PyTests"
+DATAFILE = "5erdB.pdb"
+OUTFILE = "5erdtest.pdb"
+AAS = ["ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLY", 
+      "HIS", "ILE", "LEU", "LYS", "MET", "PHE", "PRO", 
+      "SER", "THR", "TRP", "TYR", "VAL", "GLU"]
 
 def parse(datafile):
     # nothing clever here, the file parser
@@ -34,14 +36,14 @@ def welcome():
            "\n#  by Nicolas Pettican  #" + "\n#\t\t\t#\n" + "#" *25 + "\n")
 
 def lineheads(datafile):
+    # finds and prints the different elements in line[0]
     data = [line.strip().split() for line in open(datafile, 'r')]
     print "\nThe file has %s lines\n" %(len(data))
     lineheadset = set()
     for line in data:
         if line[0] != "END":
             lineheadset.add(line[0])
-    # it was 3 in the morning when I wrote this...
-    # so I can't exactly remember how
+    # prints the element types, found at line[0]
     for element in set(lineheadset):
         x = [line[0].count(element) for line in data]
         print "{x} of which are {y} elements\n".format(x=x.count(1), y=element)
@@ -51,13 +53,15 @@ def optionsbegin(pdb):
     while True:
         try:
             choice = int(raw_input("What would you like to do to the PDB file?\n" +
-                           "Here are the current options:\n" +
+                           "Here are the current options:\n\n" +
                            "Option 1 - Replace characters    # it will replace every instance of those characters\n" +
-                           "Option 2 - ATOM to HETATM        # work in progress\n" +
-                           "Option 3 - Quit\n\nOption: "))
+                           "Option 2 - ATOM to HETATM        # work in progress\n" + 
+                           "Option 3 - HETATM to ATOM        # work in progress\n" +
+                           "Option 4 - Future options        # future options will be available\n" +
+                           "Option 5 - Quit\n\nOption: "))
         except ValueError:
             continue
-        if 1 <= choice <= 3:
+        if 1 <= choice <= 5:
             return choice
 
 def findelement(pdb,busca):
@@ -65,6 +69,7 @@ def findelement(pdb,busca):
     return sum(1 for l in pdb if re.search(busca, l))
 
 def findrange(pdb,busca):
+    # finds the range/s where the query "busca" is
     rangebusca = [i for i, line in enumerate(pdb) if re.search(busca, line)]
     realranges = [map(itemgetter(1), value)
                   for i, value in groupby(enumerate(rangebusca),
@@ -74,8 +79,6 @@ def findrange(pdb,busca):
     elif len(realranges) >= 2:
         return [[realranges[i][0], realranges[i][-1] + 1]
                 for i, line in enumerate(realranges)]
-        #return [line[0], line[1] + 1 for line in realranges]
-    #return [rangebusca[0], rangebusca[-1] + 1]
 
 def findindex(pdb,busca):
     # finds what column the query string is
@@ -86,8 +89,7 @@ def findindex(pdb,busca):
     return list(temp)
 
 def replacechar(pdb):
-    # to change something within the PDB
-    # chosen by the user
+    # to change user defined characters within the PDB
     try:
         busca = raw_input("\nInsert characters you want to look for:\t")
         while not (len(busca) > 2):
@@ -114,77 +116,144 @@ def replacechar(pdb):
     except:
         print "\nWoops, can't seem to find %s's location\n" %(busca)
     # THE REPLACING SECTION
+    option = 0  # just as a default
     try:
         cambia = raw_input("Insert number or string you want to replace it with:\t")
         if len(busca) == len(cambia):
-            newpdb = replace(pdb,busca,cambia)
+            convertrange = rangebusca
+            newpdb = replace(pdb,busca,cambia,convertrange,option)
             return newpdb
         else:
             print ("\nReplacing a string of different size may break the PDB.\n")
             if continuar():
-                newpdb = replace(pdb,busca,cambia)
+                convertrange = rangebusca
+                newpdb = replace(pdb,busca,cambia,convertrange,option)
                 return newpdb
             else:
                 quitsesh()
     except:
-        print "\nWoops, something went wrong\n"
+        print "\nWoops, something went wrong replacing the string\n"
         quitsesh()
 
-def replace(pdb,busca,cambia):
+def replace(pdb,busca,cambia,convertrange,option):
     # THE REPLACING FUNCTION
     # note that this will break the pdb into individual characters
     # pdbchars has all the lines broken into individual characters
-
     pdbchars = [[l.strip() for l in line] for line in pdb]
     buscachars = [l.strip() for l in busca]
     cambiachars = [l.strip() for l in cambia]
-    rangebusca = findrange(pdb,busca)
-
     begin = 100   # just as a default
     end = 101     # just as a default
-
-    # here, some nested code
+    # goes through each character and changes under conditions
     newpdb = []
     for counter, line in enumerate(pdbchars):
         newline = []
-        b = 0
-        charcount = 0
-        checkifrange = checkinrange(counter,rangebusca)
-        # check if current row is in the the "replacing" range
-        if checkifrange[0] == True:
+        checkifrange = checkinrange(counter,convertrange)
+        # check if current row is in the range
+        if checkifrange:
             for char in line:
                 newline.append(char)
-        elif checkifrange[0] == False:
-            for char in line:
-                check = 0
-                if line[charcount] == buscachars[0] and line[charcount + 1] == buscachars[1]:
-                    for i, value in enumerate(buscachars):
-                        if line[charcount + i] == buscachars[i]:
-                            check = i
-                    if check > 1:
-                        begin = b
-                        end = b + len(buscachars)
-                        x = 0
-                        for chars in pdbchars[counter][begin:end]:
+        else:
+            if option > 0:
+                for charcount, char in enumerate(line):
+                    if charcount == 0:
+                        for x, chars in enumerate(pdbchars[counter][0:6]):
                             newline.append(char.replace(char, cambiachars[x]))
-                            x += 1
-                elif charcount not in range(begin, end):
-                    newline.append(char)
-                charcount += 1
-                b += 1
+                    elif charcount not in range(0,6):
+                        newline.append(char)
+            else:
+                for charcount, char in enumerate(line):
+                    if line[charcount] == buscachars[0] and line[charcount + 1] == buscachars[1] and line[charcount + 2] == buscachars[2]:
+                        check = [value for i, value in enumerate(buscachars) if line[charcount + i] == buscachars[i]]
+                        if len(check) > 2:
+                            begin = charcount
+                            end = charcount + len(buscachars)
+                            for x, chars in enumerate(pdbchars[counter][begin:end]):
+                                newline.append(char.replace(char, cambiachars[x]))
+                    elif charcount not in range(begin, end):
+                        newline.append(char)
         newpdb.append(newline)
 
     return newpdb
 
-def checkinrange(counter,rangebusca):
-    if len(rangebusca) == 1:
-        return [True if counter not in range(rangebusca[0][0], rangebusca[0][-1]) else False]
-    elif len(rangebusca) >= 2:
+def checkinrange(counter,convertrange):
+    # returns true if the line is not in the range
+    if len(convertrange) == 1:
+        return [True if counter not in range(convertrange[0][0], convertrange[0][-1]) else False][0]
+    elif len(convertrange) >= 2:
         isit = 0
-        for r in rangebusca:
+        for r in convertrange:
             if counter not in range(r[0], r[-1]):
                 isit += 1
-        return [True if isit == len(rangebusca) else False]
+        return [True if isit == len(convertrange) else False][0]
+    
+def atomtohet(pdb,option):
+    # to convert ATOM to HETATM
+    elementsofinterest = ["ATOM", "HETATM"]
+    data = [line.split() for line in pdb]
+    # check what column the elements are in
+    try:
+        busca = checkforadefault(pdb)
+        column = int(findindex(pdb,busca)[0] - 1)
+    except:
+        print "\nAn error occurred within checkforadefault()\n"
+    lineheadset = set()
+    # finds the elements available to substitute
+    try:
+        for line in data:
+            if len(line) > column and line[column] not in AAS and line[0] in elementsofinterest:
+                lineheadset.add(line[column])
+        for element in set(lineheadset):
+            x = [line[column].count(element) for line in data if len(line) > column]
+            print "\nFound {y} element {x} times".format(x=x.count(1), y=element)
+    except:
+        print "\nWoops, could not find elements other than amino acids\n"
+        quitsesh()
+    # user chooses which elements to substitute
+    try:
+        chosen = atomhetoptions(lineheadset)
+    except:
+        print "\nWoops, failed to find elements\n"
+        quitsesh()
+    # find the range of user-input elements
+    try:
+        rawrange = [findrange(pdb,busca) for busca in chosen]
+        convertrange = [x for i in rawrange for x in i]
+    except:
+        print "\nWoops, could not find range\n"
+    # THE REPLACING SECTION
+    if option == 1:
+        busca = "ATOM  "
+        cambia = "HETATM"
+    elif option == 2:
+        cambia = "ATOM  "
+        busca = "HETATM"
+    newpdb = replace(pdb,busca,cambia,convertrange,option)
+    return newpdb
+    
+def checkforadefault(pdb):
+    # fun fact, these bellow are the most abundant amino acids in proteins
+    # excluding of course NAG and MAN
+    defaults = ['LEU', 'SER', 'LYS', 'GLU' 'NAG', 'MAN']
+    return [i for i in defaults for line in pdb if re.search(i, line)][0]
+    
+def atomhetoptions(lineheadset):
+    # user enters the desired element/s to substitute
+    busca = raw_input("\nWhich element would you like to convert?\n" + 
+                      "You can input multiple elements separated by ','\n" + 
+                      "e.g.\tNAG, MAN\n\nElement/s: ").split(',')
+    buscaset = set()
+    tmp = [buscaset.add(i) for i in busca]
+    while not any(s in lineheadset for s in busca):
+        busca = raw_input("\nCould not find element/s, " + 
+                          "try again and enter them correctly.\n" + 
+                          "e.g. NAG\tor\tNAG, MAN" +
+                          "\n\nElement/s:").split(',')
+        buscaset = set()
+        tmp = [buscaset.add(i) for i in busca]
+    for element in set(buscaset):
+        print "\nElement %s found" %(element)
+    return [element for element in set(buscaset)]
 
 def reconstruct(newpdb):
     # reconstruct the "replaced" lines with the appropriate spacing
@@ -206,7 +275,7 @@ def outputfile(readypdb,outfile):
 
 def nothingyet():
     # just some of my nonesense :grin:
-    print ("\nCurrently working on ATOM to HETATM conversion\n" +
+    print ("\nCurrently perfecting ATOM to HETATM conversion\n" +
            "I also want to add a 'replace entire column' option,\n" +
            "but I could use some help to increase functionality,\n" +
            "so be sure to fork it on github :D\n")
@@ -232,28 +301,35 @@ def main():
         pdb = parse(datafile)
         welcome()
         lineheads(datafile)
+        # GIVE USER OPTIONS
         choice = optionsbegin(pdb)
         if choice == 1:
             newpdb = replacechar(pdb)
         elif choice == 2:
-            nothingyet()
+            option = 1
+            newpdb = atomtohet(pdb,option)
         elif choice == 3:
+            option = 2
+            newpdb = atomtohet(pdb,option)
+        elif choice == 4:
+            nothingyet()
+        elif choice == 5:
             quitsesh()
-
+        # RECONSTRUCT NEW PDB
         try:
             readypdb = reconstruct(newpdb)
             print "\n*Successfully reconstructed new PDB structure!*\n"
         except:
             print "\nWoops, something went wrong when reconstructing the PDB!\n"
             quitsesh()
-
+        # WRITE NEW PDB TO FILE
         try:
             outputfile(readypdb,outfile)
             print "*Successfully created new PDB file!*\n"
         except:
             print "\nWoops, something went wrong when creating the PDB!\n"
             quitsesh()
-
+    # if user inpyted wrong file info
     else:
         print "\nCould not open file\nMake sure you type the correct directory and file name\n"
         quitsesh()
